d3ef98dc41877a0e1328e4f180fd11dddacc1f1e
Vikas <vikas@posteo.de>
2022-07-10 17:36:31 +0200

    fix our wallet address

--- app/utils/helper.ts
+++ app/utils/helper.ts
@@ -3,7 +3,7 @@ export function classNames(...classes) {
 }
 
 export const getMumbaiTestnetProvider = () => "https://rpc-mumbai.maticvigil.com/"
-export const ourReceiverTestWallet = () => "0xE5aD97f1731174D14A71D43Bd06E94a1D3e59A4a"
+export const ourReceiverTestWallet = () => "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
 export const ourDAIxTestContractAddress = () => "0xE5aD97f1731174D14A71D43Bd06E94a1D3e59A4a"
 
 const fDaiXAddress = "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
